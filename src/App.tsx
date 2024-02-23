8ceca9c1038ed255dd20cabb73c61c6eceae8ade
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-02-23 15:39:03 +0100

    initiate commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <button>add to card</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UI/Buttons/Buttons.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function ButtonPrimary() {
+  return <button>button primary</button>;
+}
+
+export function ButtonSecondary() {
+  return <button>button secondary</button>;
+}
+
+export function ButtonOutline() {
+  return <button>button outline</button>;
+}
