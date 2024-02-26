e9c83d04356340f70b6b6996c8a071518d99fb30
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-02-26 15:09:45 +0100

    ..

--- src/components/UI/Animation/Animation.ts
+++ src/components/UI/Animation/Animation.ts
@@ -16,5 +16,5 @@ export const basketAnimation = {
   key: "searchBar",
   initial: { opacity: 0, y: -20 },
   animate: { opacity: 1, y: 0 },
-  transition: { duration: 0.5 },
+  transition: { duration: 2 },
 };
