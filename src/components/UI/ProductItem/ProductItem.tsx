0a2eb5c46d581a0cece0e8cbcbbf84355145f520
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-02-27 21:56:24 +0100

    ..

--- src/components/UI/ProductItem/ProductItem.tsx
+++ src/components/UI/ProductItem/ProductItem.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Styles from "./ProductItem.module.css";
 import { Link } from "react-router-dom";
+import favIconOutline from "./../../../assets/icons/heart-outline.svg";
+import favIconFill from "./../../../assets/icons/heart.svg";
+import addIcon from "./../../../assets/icons/add-outline.svg";
 import productImageTest from "./../../../assets/image/keyboards/NuPhy Gem80-MysticIndigox.webp";
 
 function ProductItem() {
   return (
     <div className={Styles.productItem}>
       <Link to="#">
-        <div className={Styles.productItem__action}>action</div>
+        <div className={Styles.productItem__action}>
+          <button>
+            <img src={favIconOutline} alt="" />
+          </button>
+          <button>
+            <img src={addIcon} alt="" />
+          </button>
+        </div>
         <div className={Styles.productItem__colors}>colors</div>
         <div className={Styles.productItem__header}>
           <img src={productImageTest} alt="" />
