750fbf6a3176960f6b2684bdd3cb0f29a3c246a7
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-02-27 15:48:40 +0100

    update select style in product page header

--- src/components/UI/ProductPageHeader/ProductPageHeader.tsx
+++ src/components/UI/ProductPageHeader/ProductPageHeader.tsx
@@ -11,8 +11,8 @@ function ProductPageHeader() {
         <select name="sort" id="sort">
           <option value="featured">Featured</option>
           <option value="newest">Newest</option>
-          <option value="price">Price:Lower to Higher</option>
-          <option value="price">Price:Higher to Lower</option>
+          <option value="price">Price: Lower to Higher</option>
+          <option value="price">Price: Higher to Lower</option>
           <option value="popularity">Popularity</option>
         </select>
       </div>
