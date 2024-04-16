6391ae477bfce696771e7b22fa86440dea315ae9
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2024-04-16 12:25:04 +0200

    -add font weight to index.css -add images to assets folder -add media icons to assets folder -fix media container in home page

--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -1,12 +1,62 @@
 import Row from "../../UI/Row/Row";
 import Styles from "./Home.module.css";
+import wallpaper from "../../../assets/image/mediaIcons/wallpaper_logo_f79e68fd-777d-4d65-b794-130a0534f53d_360x.jpg"
+import wired from "../../../assets/image/mediaIcons/wired_logo_cd029a23-c265-49f3-b457-22f2f9dfda27_360x.jpg"
+import MediaContainer from "./MediaContainer/MediaContainer";
 
+
+
+const icons = [
+  {
+    id:1,
+    image: wallpaper,
+  },
+  // {
+  //   id:2,
+  //   image: wired,
+  // },
+  // {
+  //   id:3,
+  //   image: wallpaper,
+  // },
+  // {
+  //   id:4,
+  //   image: wallpaper,
+  // },
+  // {
+  //   id:5,
+  //   image: wallpaper,
+  // },
+  // {
+  //   id:6,
+  //   image: wallpaper,
+  // },
+  // {
+  //   id:7,
+  //   image: wallpaper,
+  // },
+  // {
+  //   id:8,
+  //   image: wallpaper,
+  // },
+]
 function Home() {
   return (
     <section className={Styles.container}>
-      <Row>
-        <h1>Welcome to our store</h1>
-      </Row>
+      <div className={Styles.homeContainer}>
+        <div className={Styles.swiperContainer}>hi</div>
+        <div className={Styles.inStockContainer}>how</div>
+        <div className={Styles.styleContainer}>are</div>
+        <div className={Styles.accessoriesContainer}>you</div>
+        <div className={Styles.mediaContainer}>
+          <div className={Styles.title}>
+            <h2>We are on Media</h2>
+          </div>
+          <div className={Styles.iconsContainer}>
+            <MediaContainer/>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/Pages/ItemPage/Side/Side.tsx
+++ src/components/Pages/ItemPage/Side/Side.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Styles from "./Side.module.css";
 import SideHeader from "./SideHeader/SideHeader";
 import SideBody from "./SideBody/SideBody";
-import SIdeFooter from "./SideFooter/SideFooter";
+import SIdeFooter from "./SideFooter/SIdeFooter";
 import { DividerH } from "../../../UI/Divider/Divider";
 
 function Side() {
