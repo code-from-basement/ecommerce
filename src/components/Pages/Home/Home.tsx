6f9904e3a9e2975aff47b16d47c14c98c28e6b7a
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2024-04-16 12:28:21 +0200

    ...

--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -1,45 +1,7 @@
 import Row from "../../UI/Row/Row";
 import Styles from "./Home.module.css";
-import wallpaper from "../../../assets/image/mediaIcons/wallpaper_logo_f79e68fd-777d-4d65-b794-130a0534f53d_360x.jpg"
-import wired from "../../../assets/image/mediaIcons/wired_logo_cd029a23-c265-49f3-b457-22f2f9dfda27_360x.jpg"
 import MediaContainer from "./MediaContainer/MediaContainer";
 
-
-
-const icons = [
-  {
-    id:1,
-    image: wallpaper,
-  },
-  // {
-  //   id:2,
-  //   image: wired,
-  // },
-  // {
-  //   id:3,
-  //   image: wallpaper,
-  // },
-  // {
-  //   id:4,
-  //   image: wallpaper,
-  // },
-  // {
-  //   id:5,
-  //   image: wallpaper,
-  // },
-  // {
-  //   id:6,
-  //   image: wallpaper,
-  // },
-  // {
-  //   id:7,
-  //   image: wallpaper,
-  // },
-  // {
-  //   id:8,
-  //   image: wallpaper,
-  // },
-]
 function Home() {
   return (
     <section className={Styles.container}>
--- src/components/Pages/Home/MediaContainer/MediaContainer.tsx
+++ src/components/Pages/Home/MediaContainer/MediaContainer.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Styles from './MediaContainer.module.css'
-import wallpaper from "../../../../assets/image/mediaIcons/wallpaper_logo_f79e68fd-777d-4d65-b794-130a0534f53d_360x.jpg"
+import React from "react";
+import Styles from "./MediaContainer.module.css";
+import wallpaper from "../../../../assets/image/mediaIcons/wallpaper_logo_f79e68fd-777d-4d65-b794-130a0534f53d_360x.jpg";
 import wired from "../../../../assets/image/mediaIcons/wired_logo_cd029a23-c265-49f3-b457-22f2f9dfda27_360x.jpg";
 import hyp from "../../../../assets/image/mediaIcons/hypebeast_logo_cf4be27e-8e2c-40e0-a6a0-732dd9e063f6_360x.jpg";
 import milk from "../../../../assets/image/mediaIcons/designmilk_logo_d658a626-e535-452c-8428-736a6a54068a_360x.jpg";
@@ -13,70 +13,67 @@ import engodget from "../../../../assets/image/mediaIcons/engadget_logo_e3994487
 import mac from "../../../../assets/image/mediaIcons/9to5mac_logo_360x.jpg";
 import apple from "../../../../assets/image/mediaIcons/appleinsider_logo_9fde8270-d362-41ae-b718-a4ad9c2a85aa_360x.jpg";
 
+const icons = [
+  {
+    id: 1,
+    image: wallpaper,
+  },
+  {
+    id: 2,
+    image: wired,
+  },
+  {
+    id: 3,
+    image: hyp,
+  },
+  {
+    id: 4,
+    image: milk,
+  },
+  {
+    id: 5,
+    image: net,
+  },
+  {
+    id: 6,
+    image: forbes,
+  },
+  {
+    id: 7,
+    image: time,
+  },
+  {
+    id: 8,
+    image: the,
+  },
+  {
+    id: 9,
+    image: short,
+  },
+  {
+    id: 10,
+    image: engodget,
+  },
+  {
+    id: 11,
+    image: mac,
+  },
+  {
+    id: 12,
+    image: apple,
+  },
+];
+
 function MediaContainer() {
-
-    const icons = [
-        {
-          id:1,
-          image: wallpaper,
-        },
-        {
-          id:2,
-          image: wired,
-        },
-        {
-          id:3,
-          image: hyp,
-        },
-        {
-          id:4,
-          image: milk,
-        },
-        {
-          id:5,
-          image: net,
-        },
-        {
-          id:6,
-          image: forbes,
-        },
-        {
-          id:7,
-          image: time,
-        },
-        {
-          id:8,
-          image: the,
-        },
-        {
-            id:9,
-            image: short,
-          },
-          {
-            id:10,
-            image: engodget,
-          },
-          {
-            id:11,
-            image: mac,
-          },
-          {
-            id:12,
-            image: apple,
-          },
-      ]
-
   return (
     <div className={Styles.mainContainer}>
-
-            {icons.map((icon) => (
-                <div key={icon.id}>
-                    <img src={icon.image} alt="icon" className={Styles.iconContainer}/>
-                </div>
-            ))}
-
+      {icons.map((icon) => (
+        <div key={icon.id}>
+          <img src={icon.image} alt="icon" className={Styles.iconContainer} />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default MediaContainer
+export default MediaContainer;
