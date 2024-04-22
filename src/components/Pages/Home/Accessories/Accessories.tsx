9a16f8344924e58c1ffcd8347ffa3cbcaa563609
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2024-04-22 08:23:59 +0200

    ...

--- src/components/Pages/Home/Accessories/Accessories.tsx
+++ src/components/Pages/Home/Accessories/Accessories.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Styles from './Accessories.module.css'
 import ItemAccessories from './ItemAccessories/ItemAccessories'
 
 function Accessories() {
+  useEffect(() => {
+    const fetchItems = async () => {
+      const response = await fetch('http://127.0.0.1:5555/api/products/accessories')
+      const data = await response.json()
+      console.log(data)
+    }
+    fetchItems();
+  }, [])
   return (
     <div className={Styles.accessoriesContainer}>
         <h2>Accessories</h2>
         <div className={Styles.accessoriesItems}>
-            <ItemAccessories/>     
+            {/* <ItemAccessories/>      */}
+            
         </div>
     </div>
   )
--- src/components/Pages/ItemPage/Side/Side.tsx
+++ src/components/Pages/ItemPage/Side/Side.tsx
@@ -1,7 +1,7 @@
 import { DividerH } from "../../../UI/Divider/Divider";
 import Styles from "./Side.module.css";
 import SideBody from "./SideBody/SideBody";
-import SideFooter from "./SideFooter/SideFooter";
+import SideFooter from "./SideFooter/SIdeFooter";
 import SideHeader from "./SideHeader/SideHeader";
 
 function Side() {
