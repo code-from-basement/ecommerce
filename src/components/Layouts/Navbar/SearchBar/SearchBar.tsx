0da1e05ac65e41ae29b139e54de926988aeb33f4
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-04-23 15:02:14 +0200

    install SWR package donw with search bar input on lcick and press enter to search , and also on click of search button

--- src/components/Layouts/Navbar/SearchBar/SearchBar.tsx
+++ src/components/Layouts/Navbar/SearchBar/SearchBar.tsx
@@ -1,11 +1,15 @@
 import { motion } from "framer-motion";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import searchIcon from "../../../../assets/icons/search-outline.svg";
 import { useGlobalContext } from "../../../../context/globalContext";
 import { searchBarAnimation } from "../../../UI/Animation/Animation";
 import closeIcon from "./../../../../assets/icons/close-outline.svg";
 import Styles from "./SearchBar.module.css";
-import { useNavigate } from "react-router-dom";
+
 function SearchBar() {
+  // Ref for search input
+  const searchInputRef = useRef<HTMLInputElement>(null);
   const navigate = useNavigate();
   const { setUiToggle } = useGlobalContext();
 
@@ -16,12 +20,38 @@ function SearchBar() {
     });
   };
 
-  const onClickSearchButtonHandler = () => {
+  // Search button logic
+  const onClickSearchButtonHandler = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
     navigate("search");
+    setUiToggle((prevData: any) => {
+      return { ...prevData, isLoadingFullViewShow: true };
+    });
+    try {
+      const searchValue = searchInputRef.current?.value;
+      const response = await fetch(`http://127.0.0.1:5555/api/search?q=${searchValue}`);
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err, "error in search button");
+    } finally {
+      searchInputRef.current!.value = "";
+      setTimeout(() => {
+        setUiToggle((prevData: any) => {
+          return { ...prevData, isLoadingFullViewShow: false };
+        });
+      }, 2000);
+    }
   };
   return (
     <motion.div className={Styles.searchContainer} {...searchBarAnimation}>
-      <input className={Styles.searchInput} type="text" placeholder="Search" />
+      <input
+        className={Styles.searchInput}
+        type="text"
+        placeholder="Search here..."
+        ref={searchInputRef}
+        onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => e.key === "Enter" && onClickSearchButtonHandler(e)}
+      />
       <button className={Styles.BtnSearch} onClick={onClickSearchButtonHandler}>
         <img src={searchIcon} alt="" />
       </button>
