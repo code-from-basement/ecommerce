558b203d7acee0183f00635b14a009a7794a4709
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-04-25 13:02:32 +0200

    ..

--- src/components/Pages/ItemPage/ItemPage.tsx
+++ src/components/Pages/ItemPage/ItemPage.tsx
@@ -7,7 +7,6 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 function ItemPage() {
   const location = useLocation();
   const url = `${location.pathname}`;
-  console.log(url);
 
   const { data } = useSWR(`http://127.0.0.1:5555/api/products${url}`, fetcher, {});
   console.log(data, "data");
