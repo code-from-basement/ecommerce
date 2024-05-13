9a9b0d4e84fc335e8a030564c8240fe29ab93cb9
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2024-05-13 13:55:06 +0200

    remove fetching five items from globalContext

--- src/components/Pages/Home/Keyboards/InStockKeyboards.tsx
+++ src/components/Pages/Home/Keyboards/InStockKeyboards.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
-import Styles from './InStockKeyboards.module.css'
-import StockKeyboardItems from './InStockKeyboardItems/StockKeyboardItems'
-import { useGlobalContext } from '../../../../context/globalContext';
+import React, { useEffect, useState } from "react";
+import Styles from "./InStockKeyboards.module.css";
+import StockKeyboardItems from "./InStockKeyboardItems/StockKeyboardItems";
 
 function InStockKeyboards() {
-  const { fiveKeyboardsItems } = useGlobalContext();
+
+  //fetch for first five accessories in home page
+  const [fiveKeyboardsItems, setFiveKeyboardsItems] = useState([]);
+
+  useEffect(() => {
+    const fetchKeyboardsItems = async () => {
+      const res = await fetch("http://127.0.0.1:5555/api/products/keyboards?first=5");
+      const data = await res.json();
+      const keyboardsItems = await data.data;
+      console.log(keyboardsItems, "keyboardsItems");
+      setFiveKeyboardsItems(keyboardsItems);
+    };
+    fetchKeyboardsItems();
+  }, []);
+  
   return (
     <div className={Styles.keyboardsContainer}>
       <h2>In Stock Keyboards</h2>
-        <div className={Styles.keyboardsItems}>
-          {fiveKeyboardsItems.map((item:any, index:number) => (
-                <StockKeyboardItems key={index} item={item} />
-            ))}
-        </div>
+      <div className={Styles.keyboardsItems}>
+        {fiveKeyboardsItems.map((item: any, index: number) => (
+          <StockKeyboardItems key={index} item={item} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default InStockKeyboards
+export default InStockKeyboards;
--- src/context/globalContext.tsx
+++ src/context/globalContext.tsx
@@ -17,62 +17,12 @@ const GlobalContextProvider = ({ children }: { children: React.ReactNode }) => {
     isLoadingFullViewShow: false,
   });
 
-  //fetch for first five accessories in home page
-  const [fiveAccessoriesItems, setFiveAccessoriesItems] = useState([]);
-
-  //fetch for first five accessories in home page
-  const [fiveKeyboardsItems, setFiveKeyboardsItems] = useState([]);
-
-  //fetch for first five styles in home page
-  const [fiveStylesItems, setFiveStylesItems] = useState([]);
-
-  //fetch for first five accessories in home page
-  useEffect(() => {
-    const fetchAccessoriesItems = async () => {
-      const response = await fetch("http://127.0.0.1:5555/api/products/accessories?first=5");
-      const data = await response.json();
-      const accessoriesItems = await data.data;
-      console.log(accessoriesItems, "accessoriesItems");
-      setFiveAccessoriesItems(accessoriesItems);
-    };
-    fetchAccessoriesItems();
-  }, []);
-
-  // fetch for five first keyboards in home page
-  useEffect(() => {
-    const fetchKeyboardsItems = async () => {
-      const res = await fetch("http://127.0.0.1:5555/api/products/keyboards?first=5");
-      const data = await res.json();
-      const keyboardsItems = await data.data;
-      console.log(keyboardsItems, "keyboardsItems");
-      setFiveKeyboardsItems(keyboardsItems);
-    };
-    fetchKeyboardsItems();
-  }, []);
-
-    // fetch for five first styles item in home page
-    useEffect(() => {
-      const fetchKeyboardsItems = async () => {
-        const res = await fetch("http://127.0.0.1:5555/api/products/keycaps?first=5");
-        const data = await res.json();
-        const stylesItems = await data.data;
-        setFiveStylesItems(stylesItems);
-      };
-      fetchKeyboardsItems();
-    }, []);
-
 
   return (
     <GlobalContext.Provider
       value={{
         uiToggle,
-        fiveAccessoriesItems,
-        fiveKeyboardsItems,
-        fiveStylesItems,
         setUiToggle,
-        setFiveAccessoriesItems,
-        setFiveKeyboardsItems,
-        setFiveStylesItems,
       }}
     >
       {children}
