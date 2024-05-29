fea366ce27046a2ad75b4a15c3c897268292aaf9
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2024-05-29 15:35:26 +0200

    fix the switches and Accessories pages

--- src/components/Pages/Switches/Switches.tsx
+++ src/components/Pages/Switches/Switches.tsx
@@ -1,12 +1,32 @@
 import React from "react";
 import Styles from "./Switches.module.css";
-import Row from "../../UI/Row/Row";
+import ProductPageGrid from "../../UI/ProductPageGrid/ProductPageGrid";
+import ProductPageHeader from "../../UI/ProductPageHeader/ProductPageHeader";
+import ProductPageListItem from "../../UI/ProductPageListItem/ProductPageListItem";
+import { useLocation } from "react-router-dom";
+import useSWR from "swr";
+import Filters from "../../UI/ProductPageFilter/ProductPageFilter";
+
+
 function Switches() {
+
+  const fetcher = (url:string) => fetch(url).then((res)=>res.json());
+
+  const location = useLocation();
+  const url = `${location.pathname}${location.search}`;
+  const {data} = useSWR([`http://127.0.0.1:5555/api/products/${url}`], fetcher, {
+    revalidateIfStale: false,
+  });
+
+  console.log(data, "data from switches");
+
   return (
     <div className={Styles.switches}>
-      <Row>
-        <h1>Switches</h1>
-      </Row>
+      <ProductPageGrid>
+        <Filters />
+        <ProductPageHeader {...data} />
+        <ProductPageListItem {...data} />
+      </ProductPageGrid>
     </div>
   );
 }
