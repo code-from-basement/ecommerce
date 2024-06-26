b6edc9726326ce41edf1440456b3410b4a4583d1
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2024-06-26 09:41:04 +0200

    fixed add icon and fav icon in searchBar page

--- src/components/UI/SearchProductItem/SearchProductItem.tsx
+++ src/components/UI/SearchProductItem/SearchProductItem.tsx
@@ -1,25 +1,23 @@
 import Styles from "./SearchProductItem.module.css";
 import { fadeInSearchProductItemAnimation } from "../Animation/Animation";
-import favIconOutline from "./../../../assets/icons/heart-outline.svg";
-import addIcon from "./../../../assets/icons/add-outline.svg";
-import favIconFill from "./../../../assets/icons/heart.svg";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import StarsBar from "../StarsBar/StarsBar";
+import useAddToBasket from "../../../hooks/useAddToBasket";
+import { HeartIcon, LoaderCircle, PlusIcon } from "lucide-react";
 export default function SearchProductItem({ data }: any) {
+  const { addToBasket, error, isLoading } = useAddToBasket();
   const { _id, title, price, images, rate_average, description, new: isNew, colors } = data;
 
   return (
     <motion.div {...fadeInSearchProductItemAnimation} className={Styles.productItem}>
+      <div className={Styles.productItem__action}>
+        <button onClick={() => addToBasket(data)}>{isLoading ? <LoaderCircle className={Styles.spinner} /> : <PlusIcon />}</button>
+        <button>
+          <HeartIcon />
+        </button>
+      </div>
       <Link to={`/${title}`}>
-        <div className={Styles.productItem__action}>
-          <button id={_id}>
-            <img src={favIconOutline} alt="" />
-          </button>
-          <button id={_id}>
-            <img src={addIcon} alt="" />
-          </button>
-        </div>
         <div className={Styles.productItem__tag}>{isNew ? <span className={Styles.tagNew}>New</span> : ""}</div>
         {/* <span className={Styles.tagDiscontinue}>discontinued</span> */}
 
@@ -27,9 +25,7 @@ export default function SearchProductItem({ data }: any) {
           {colors.length !== 0 && (
             <div className={Styles.productItem__colors}>
               {colors?.map((color: any, index: number) => {
-                return (
-                  <span key={index} className={Styles.colors__block} style={{ backgroundColor: `${color.hex}` }}></span>
-                );
+                return <span key={index} className={Styles.colors__block} style={{ backgroundColor: `${color.hex}` }}></span>;
               })}
             </div>
           )}
