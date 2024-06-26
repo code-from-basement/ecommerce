a50d14c844291eb92d7e89490cf930d2114b56ea
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2024-06-26 09:13:35 +0200

    fixed add icon and fav icon in the home page

--- src/components/Pages/Home/Accessories/ItemAccessories/ItemAccessories.tsx
+++ src/components/Pages/Home/Accessories/ItemAccessories/ItemAccessories.tsx
@@ -2,41 +2,40 @@ import React, { useState } from "react";
 import Styles from "./ItemAccessories.module.css";
 import { Link } from "react-router-dom";
 import StarsBar from "../../../../UI/StarsBar/StarsBar";
-import favIconOutline from "../../../../../assets/icons/heart-outline.svg";
-import addIcon from "./../../../../../assets/icons/add-outline.svg";
+import useAddToBasket from "../../../../../hooks/useAddToBasket";
+import { motion } from "framer-motion";
+import { fadeInAnimation } from "../../../../UI/Animation/Animation";
+import { HeartIcon, LoaderCircle, PlusIcon } from "lucide-react";
 
 function ItemAccessories({ item }: any) {
+  const { addToBasket, error, isLoading } = useAddToBasket();
+  const { _id, title, new: isNew } = item;
   return (
-    <div className={Styles.itemContainer}>
-      <Link to={`/${item.title}`}>
-        <div className={Styles.itemContainer__action}>
-          <button>
-            <img src={favIconOutline} alt="" />
-          </button>
-          <button>
-            <img src={addIcon} alt="" />
-          </button>
-        </div>
-
-        <div className={Styles.itemContainer__tag}>{item.new === true ? <span className={Styles.tagNew}>New</span> : null}</div>
- 
+    <motion.div {...fadeInAnimation} className={Styles.itemContainer}>
+      <div className={Styles.itemContainer__action}>
+        <button onClick={() => addToBasket(item)}>{isLoading ? <LoaderCircle className={Styles.spinner} /> : <PlusIcon />}</button>
+        <button>
+          <HeartIcon />
+        </button>
+      </div>
+      <Link to={`/${title}`}>
+        <div className={Styles.itemContainer__tag}>{isNew ? <span className={Styles.tagNew}>New</span> : ""}</div>
         <div className={Styles.itemContainer_header}>
           <img src={`/src/assets/image/${item.images[0]}`} alt={item.title} />
           <div className={Styles.itemContainer__colors}>
-          {item.colors.map((color: any, index: number) => {
-            return <span key={index} className={Styles.colors__block} style={{ backgroundColor: `${color.hex}` }}></span>;
-          })}
-        </div>
+            {item.colors.map((color: any, index: number) => {
+              return <span key={index} className={Styles.colors__block} style={{ backgroundColor: `${color.hex}` }}></span>;
+            })}
+          </div>
         </div>
 
         <div className={Styles.itemContainer__footer}>
           <h2>{item.title}</h2>
-          {/* <p>QMK/VIA Wireless Custom Mechanical Keyboard</p> */}
           <StarsBar rate_average={item.rate_average} />
           <span>${item.price}</span>
         </div>
       </Link>
-    </div>
+    </motion.div>
   );
 }
 
--- src/components/Pages/Home/YourStyle/StylesItems/StyleItems.tsx
+++ src/components/Pages/Home/YourStyle/StylesItems/StyleItems.tsx
@@ -2,23 +2,24 @@ import React from "react";
 import Styles from "./StyleItems.module.css";
 import { Link } from "react-router-dom";
 import StarsBar from "../../../../UI/StarsBar/StarsBar";
-import favIconOutline from "../../../../../assets/icons/heart-outline.svg";
-import addIcon from "./../../../../../assets/icons/add-outline.svg";
+import useAddToBasket from "../../../../../hooks/useAddToBasket";
+import { motion } from "framer-motion";
+import { fadeInAnimation } from "../../../../UI/Animation/Animation";
+import { HeartIcon, LoaderCircle, PlusIcon } from "lucide-react";
 
 function StyleItems({ item }: any) {
+  const { addToBasket, error, isLoading } = useAddToBasket();
+  const { _id, title, new: isNew } = item;
   return (
-    <div className={Styles.itemContainer}>
-      <Link to={`/${item.title}`}>
-        <div className={Styles.itemContainer__action}>
-          <button>
-            <img src={favIconOutline} alt="" />
-          </button>
-          <button>
-            <img src={addIcon} alt="" />
-          </button>
-        </div>
-
-        <div className={Styles.itemContainer__tag}>{item.new === true ? <span className={Styles.tagNew}>New</span> : null}</div>
+    <motion.div {...fadeInAnimation} className={Styles.itemContainer}>
+      <div className={Styles.itemContainer__action}>
+        <button onClick={() => addToBasket(item)}>{isLoading ? <LoaderCircle className={Styles.spinner} /> : <PlusIcon />}</button>
+        <button>
+          <HeartIcon />
+        </button>
+      </div>
+      <Link to={`/${title}`}>
+        <div className={Styles.itemContainer__tag}>{isNew ? <span className={Styles.tagNew}>New</span> : ""}</div>
 
         <div className={Styles.itemContainer_header}>
           <img src={`/src/assets/image/${item.images[0]}`} alt={item.title} />
@@ -37,7 +38,7 @@ function StyleItems({ item }: any) {
           <span>${item.price}</span>
         </div>
       </Link>
-    </div>
+    </motion.div>
   );
 }
 
